603ef44aa98b9a8e9dee424d1d339052bd16dcf9
Ahmad Faiz <afaiz.iub@gmail.com>
2024-02-06 00:17:33 -0500

    initial commit

---
+++ memcache_lite/run_test1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+./client set A 20 sdgjhgdgd
+./client set A 20 854uV54wz50544354dg
+./client set B 20 6554k543l5444544weg
+./client set C 20 4555445445540544egd
+./client set D 20 4554544554445445dfdf
+./client set E 20 5544554455445544v
+./client set kk 40 sdgnkfjbgkn
+./client get A
+./client get P
+./client get kk
+./client get A
+./client set C 20 dgsjdgnkjndg
+./client set R 40 lsdghkjdsghboijfkngzdough
+./client set S 40 lsdghkjdsghboijfkngzdough
---
+++ memcache_lite/run_test2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./client get A
+./client get P
+./client get kk
+./client get A
+./client get D
+./client get kk
+./client get E
