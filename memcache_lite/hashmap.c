a32a877678d54672bf9048dbf7f09585a7504ef4
Ahmad Faiz <afaiz.iub@gmail.com>
2024-02-06 00:13:20 -0500

    added dequeue_hash and delete_node

--- memcache_lite/hashmap.c
+++ memcache_lite/hashmap.c
@@ -3,19 +3,10 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include "hashmap.h"
 
-#define TABLESIZE 6
-#define MAXKEYSIZE 100
-
-typedef struct kv_node {
-    const char *key;
-    const char *value;
-    size_t value_size;
-    int user_size;
-    struct kv_node *next;
-} kv_node;
-
-kv_node * hash_map[TABLESIZE];
+node_hash_t *head_hash = NULL;
+node_hash_t *tail_hash = NULL;
 
 void init_map() {
     for (int i = 0; i < TABLESIZE; i++) {
@@ -33,65 +24,84 @@ uint32_t hash_string(const char *str) {
     return hash % TABLESIZE;
 }
 
-kv_node *create_node(const char *key, int user_size, const char *value) {
-    kv_node *tmp = (kv_node *)malloc(sizeof(kv_node)); // assign heap
-    if (tmp == NULL) {
-        perror("create: kvNode");
+kv_node_t *create_node(const char *key, size_t user_size, char *value, unsigned char oper_flg) {
+    kv_node_t *node = (kv_node_t *)malloc(sizeof(kv_node_t)); // assign heap
+    if (node == NULL) {
+        perror("kvNode heap");
         exit(1);
     }
-    tmp->key = key;
-    tmp->value = value;
-    tmp->value_size = strlen(value);
-    tmp->user_size = user_size;
-    tmp->next = NULL;
-    return tmp;
+    node->key = strdup(key);
+    node->value = strdup(value);
+    node->value_size = strlen(value);
+    node->user_size = user_size;
+    node->oper_flg = oper_flg; // 0 for get, 1 for set
+    node->next = NULL;
+    return node;
 }
 
-bool insert_node(kv_node *kv_ptr) {
+bool insert_node(kv_node_t *kv_ptr) {
     if (kv_ptr == NULL) return false;
-    uint32_t index = hash_string(kv_ptr->key);
+    char *key_copy = strdup(kv_ptr->key);
+    uint32_t index = hash_string(key_copy);
     kv_ptr->next = hash_map[index];
     hash_map[index] = kv_ptr; // insert to head of queue
+    // free(kv_ptr);  // never free if not making a struct copy
     return true;
 }
 
-kv_node *get_node(const char *key) {
+kv_node_t *get_node(const char *key) {
     if (key == NULL) return NULL;
-    uint32_t index = hash_string(key);
-    kv_node *current = hash_map[index];
-    while(current != NULL && (strncmp(current->key, key, MAXKEYSIZE) != 0)) {
+    uint32_t idx = hash_string(key);
+    int index = idx;
+    if (hash_map[index] == NULL) {
+        return NULL;
+    }
+    kv_node_t *current = hash_map[index];
+    size_t key_len = strlen(key);
+    while(current != NULL && (strncmp(current->key, key, key_len) != 0)) {
+        printf("current key:%s\n", current->key);
         current = current->next;
     }
     return current;
 }
 
-bool delete_node(kv_node *kv_ptr) {
-    if (kv_ptr == NULL) return false;
-    uint32_t index = hash_string(kv_ptr->key);
-    kv_node *current = hash_map[index];
-    kv_node *prev = NULL;
-    while (current->next != NULL && (strncmp(current->key, kv_ptr->key, MAXKEYSIZE) != 0)) {
-        prev = current;
-        current = current->next;
+void enqueue_hash(kv_node_t *tmp, int sock_fd) {
+    node_hash_t *new_node = (node_hash_t*)malloc(sizeof(node_hash_t));
+    if (new_node == NULL) {
+        perror("error allocaating node_hash_t");
+        return;
     }
-    if (current == NULL) return false; // not found
-    if (prev == NULL) {
-        hash_map[index] = current->next; // delete head
+    new_node->data = tmp;
+    new_node->next = NULL;
+    new_node->sock_fd = sock_fd;
+    if (tail_hash == NULL) { 
+        head_hash = new_node; // empty queue
     } else {
-        prev->next = current->next;
+        tail_hash->next = new_node; // fifo
     }
-    return true;
+    tail_hash = new_node; // update tail
 }
 
+node_hash_t *dequeue_hash() {
+    if (head_hash == NULL) { // empty
+        return NULL;
+    } else {
+        node_hash_t *tmp = head_hash;
+        head_hash = head_hash->next;
+        if (head_hash == NULL) tail_hash = NULL;
+        // free(tmp);
+        return tmp; // pointer to kv_node_t
+    }
+}
 
-void print_table() {
+void print_table(void) {
     printf("TABLE START\n");
     for(int i = 0; i < TABLESIZE; i++) {
         if (hash_map[i] == NULL) {
             printf("\t%i\t---\n", i);
         } else {
             printf("\t%i--", i);
-            kv_node *tmp = hash_map[i];
+            kv_node_t *tmp = hash_map[i];
             while(tmp != NULL) {
                 printf("%s--", tmp->key);
                 tmp = tmp->next;
@@ -102,21 +112,47 @@ void print_table() {
     printf("TABLE END\n");
 }
 
+bool delete_node(kv_node_t *kv_ptr) {
+    if (kv_ptr == NULL) return false;
+    char *key_copy = strdup(kv_ptr->key);
+    size_t key_len = strlen(key_copy);
+    uint32_t index = hash_string(key_copy);
+    if (hash_map[index] == NULL) return false;
+    kv_node_t *current = hash_map[index];
+    kv_node_t *prev = NULL;
+    if (head_hash == NULL) return false;
+    while ((strncmp(key_copy, current->key, key_len) != 0) && (current != NULL)) {
+        prev = current;
+        current = current->next;
+
+    }
+    if (current == NULL) return false;
+    if (prev == NULL){
+        hash_map[index] = current->next; // delete head
+    } else { 
+        prev->next = current->next;
+        free(current); // free
+    }
+    return true;
+}
+/*
 int main(void) {
     
     init_map();
 
-    kv_node *a, *b, *c, *d, *e, *f, *g, *h, *i, *j;
-    a = create_node("A", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    b = create_node("B", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    c = create_node("C", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    d = create_node("D", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    e = create_node("E", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    f = create_node("F", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    g = create_node("G", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    h = create_node("H", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    i = create_node("I", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
-    j = create_node("J", 20, "dfsdksgjhdgnsvnitrungjnbskgb");
+    kv_node_t *a, *b, *c, *d, *e, *f, *g, *h, *i, *j;
+    a = create_node("A", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    printf("%s %zu %zu %s %u\n", a->key, a->user_size, a->value_size, a->value, a->oper_flg);
+    b = create_node("B", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    c = create_node("C", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    d = create_node("D", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    e = create_node("E", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    printf("%s %zu %zu %s\n", e->key, e->user_size, e->value_size, e->value);
+    f = create_node("F", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    g = create_node("G", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    h = create_node("H", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    i = create_node("I", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
+    j = create_node("J", 20, "dfsdksgjhdgnsvnitrungjnbskgb", 1);
 
     printf("%s\n", a->key);
     print_table();
@@ -128,6 +164,28 @@ int main(void) {
     insert_node(f);
     insert_node(g);
     print_table();
+    printf("after print()");
+    delete_node(g);
+    print_table();
+    insert_node(h);
+    insert_node(i);
+    insert_node(j);
+    // print_table();
+    printf("before get\n");
+    get_node(a->key);
+    printf("after get\n");
+    printf("%s\n", a->key);
+
+    // free(a);
+    // free(b);
+    // free(c);
+    // free(d);
+    // free(e);
+    // free(f);
+    // free(h);
+    // free(i);
+    // free(j);
 
     return 0;
 }
+*/
