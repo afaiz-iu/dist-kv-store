70fd67f14f53b7782e994d706232ead4c298a553
Ahmad Faiz <afaiz@iu.edu>
2024-03-30 23:51:41 -0400

    config CMake for grpc based MapReduce system

---
+++ MapReduce/cmake/build/CMakeFiles/mapreduce_grpc_proto.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mapreduce_grpc_proto.dir/mapreduce.grpc.pb.cc.o"
+  "CMakeFiles/mapreduce_grpc_proto.dir/mapreduce.grpc.pb.cc.o.d"
+  "CMakeFiles/mapreduce_grpc_proto.dir/mapreduce.pb.cc.o"
+  "CMakeFiles/mapreduce_grpc_proto.dir/mapreduce.pb.cc.o.d"
+  "libmapreduce_grpc_proto.a"
+  "libmapreduce_grpc_proto.pdb"
+  "mapreduce.grpc.pb.cc"
+  "mapreduce.grpc.pb.h"
+  "mapreduce.pb.cc"
+  "mapreduce.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapreduce_grpc_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MapReduce/cmake/build/CMakeFiles/mapreduce_grpc_proto.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libmapreduce_grpc_proto.a"
+)
